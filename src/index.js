428b084c4cf715f9ae8dfa8380f8db4b8a6d96b2
Ashish Kayastha <akayastha@adobe.com>
2022-07-13 00:13:31 -0500

    Added prompting function.

--- src/index.js
+++ src/index.js
@@ -30,6 +30,25 @@ class TheGenerator extends Generator {
   async initializing () {
   }
 
+  async prompting() {
+    const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?"
+      }
+    ]);
+
+    this.log("app name", answers.name);
+    this.log("cool feature", answers.cool);
+  }
+
   async writing () {
     const destFolder = this.props.destFolder
     this.sourceRoot(path.join(__dirname, './templates/'))
